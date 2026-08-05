7fbcd5719388128beffd595a4c338e4f9f24e6db
Amir36o <batamirmohammad@gmail.com>
2026-08-05 22:11:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print("DarkVibes Bot is ready!")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🌑 سلام! به DarkVibes Bot خوش آمدی 🖤")
+
+app = Application.builder().token("TOKEN").build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
